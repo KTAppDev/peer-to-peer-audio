5182add9c4c85eba4c0fa436b87ce28840440d5a
Ken Taylor <kentaylosappdev@gmail.com>
2023-12-22 10:56:52 -0400

    init

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Created project, github.com/ktappdev/peer_to_peer_audio")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ktappdev/peer_to_peer_audio
+
+go 1.21.5
